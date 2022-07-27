b57a0f09f60d63a86992817633e995144317f0bb
ductruong253 <ddtruong94@gmail.com>
2022-07-27 23:06:58 +0700

    First commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const SERVICE_NAME = process.env.SERVICE_NAME;
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+  while(true) {
+    console.log(`${SERVICE_NAME} is running sucessfully!`);
+    await sleep(5000);
+  }
+}
+
+main();
